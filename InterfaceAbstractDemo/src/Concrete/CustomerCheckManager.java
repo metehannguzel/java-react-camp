2e4743c70ec2b7062632f89ef5389c04d0f15a96
Metehan <66705106+metehannguzel@users.noreply.github.com>
2021-05-03 18:42:08 +0300

    initial commit

---
+++ InterfaceAbstractDemo/src/Concrete/CustomerCheckManager.java
@@ -0,0 +1,13 @@
+package Concrete;
+
+import Abstract.ICustomerCheckService;
+import Entities.Customer;
+
+public class CustomerCheckManager implements ICustomerCheckService{
+	@Override
+	public boolean checkIfRealPerson(Customer customer) {
+		return true;
+	}
+	
+	
+}
---
+++ InterfaceAbstractDemo/src/Concrete/StarbucksCustomerManager.java
@@ -0,0 +1,27 @@
+package Concrete;
+
+import Abstract.BaseCustomerManager;
+import Abstract.ICustomerCheckService;
+import Entities.Customer;
+
+public class StarbucksCustomerManager extends BaseCustomerManager {
+	private ICustomerCheckService customerCheckService;
+	
+	public StarbucksCustomerManager() {
+		
+	}
+	
+	public StarbucksCustomerManager(ICustomerCheckService customerCheckService) {
+		this.customerCheckService = customerCheckService;
+	}
+	
+	@Override
+	public void save(Customer customer) {
+		if(customerCheckService.checkIfRealPerson(customer))
+			super.save(customer);
+		else
+			System.out.println("Not a valid person!");
+	}
+	
+	
+}
